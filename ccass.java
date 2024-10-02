c5d72961d9adc29b08937eb20c9a130926e78f05
Ayush Khadgi <107793290+Ayushkhadgi@users.noreply.github.com>
2024-10-02 15:25:28 +0530

    portfolio

---
+++ ccass.java
@@ -0,0 +1,8 @@
+import java.lang.ClassNotFoundException;
+class Mega
+{
+    public static void main(String[] args) {
+        //int temp,r,n,sum=0;
+        System.out.println("e");
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+Class Mega
+{
+    public static void main(String[] args) {
+        int temp,r,n,sum=0;
+        system.out.println("he");
+    }
+}
